2c59401f6815c7b280138ac1f16a6c078866370a
Jean-Gabriel BARTHELEMY <j-g.barthelemy@linguacustodia.com>
2025-01-21 14:52:36 +0100

    Debug efficient implementation of Diff Attention

--- examples/gpt3/train_gpt3_175b_distributed.sh
+++ examples/gpt3/train_gpt3_175b_distributed.sh
@@ -37,7 +37,7 @@ GPT_MODEL_ARGS=(
 
 TRAINING_ARGS=(
     --num-workers 16
-    --micro-batch-size 5
+    --micro-batch-size 4
     --train-samples 12207050 
     --weight-decay 0.1 
     --adam-beta1 0.9 
